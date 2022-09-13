fe9b76058fda76b7c2ab8815a7902ab2fb471c64
GabrielBeltran7 <gabrielbeltranmedina@gmail.com>
2022-09-13 08:08:13 -0500

    01-IntroToCS

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,11 +2,27 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  var array = num.split ("")
+  var sum = 0;
+  for (let i = 0; i <array.length; i++){
+    sum = sum + array[i] * 2 ** (array.length-1-i);
+   
+  }
+  return sum
+
 
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+ let resto = "";
+
+while (num !==0 ){
+  resto= num %2 +resto 
+  num = Math.floor(num/2)
+}
+  
+ return resto 
 
 }
 
